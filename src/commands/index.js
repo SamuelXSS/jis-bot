f1cd301263d7f0450ce05e5c50ad0eef28bbdbfa
SamuelXSS <samuelxses@unipam.edu.br>
2020-12-28 20:35:58 -0300

    fix: fixed archive patterns

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,6 +1,14 @@
-//import commands here
-
+const ping = require('./ping')
 
 const commands = {
+  ping
+}
 
+module.exports = async (msg) => {
+  const args = msg.content.split(' ')
+  if (args.length == 0 || args[0].charAt(0) !== '!') return
+  const command = args.shift().substr(1)
+  if (Object.keys(commands).includes(command)) {
+    commands[command](msg, args)
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require('discord.js')
+const client = new Discord.Client()
 require('dotenv').config()
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
+  console.log(`Logged in as ${client.user.tag}!`)
+})
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
-    }
-});
+  if (msg.content === 'ping') {
+    msg.reply('pong')
+  }
+})
 
-client.login(process.env.DISCORD_SECRET);
+client.login(process.env.DISCORD_SECRET)
