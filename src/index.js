4d4e6e8323d577c0f18684b3964e33ac2076da1d
SamuelXSS <samuelxses@unipam.edu.br>
2020-12-28 20:37:55 -0300

    added: command handler

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const commandHandler = require('./commands')
+
 require('dotenv').config()
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong')
-  }
-})
+client.on('message', commandHandler)
 
 client.login(process.env.DISCORD_SECRET)
